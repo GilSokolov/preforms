7638a920fa9db6236ba39b68f0b5a4c071f6366f
Gil Sokolov <gilsokolov.gs@gmail.com>
2026-03-27 14:58:20 +0200

    add homepage link

--- example-app/src/app/layout/toolbar/toolbar.component.ts
+++ example-app/src/app/layout/toolbar/toolbar.component.ts
@@ -1,12 +1,13 @@
 import { AsyncPipe, NgClass } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { SidenavService } from '../../services/sidenav.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
-  imports: [NgClass, AsyncPipe],
+  imports: [NgClass, AsyncPipe, RouterLink],
 })
 export class ToolbarComponent {
   sidenav = inject(SidenavService);
